fa7ca72a645b95a04750157759cc10d93d4e0435
Moeckel, Nickolas <Nickolas.Moeckel@windlogics.com>
2016-12-15 16:00:25 -0600

    switch to webpack2

--- client/index.js
+++ client/index.js
@@ -1,21 +1,21 @@
-require('bootstrap/dist/css/bootstrap.min.css');
-require('./js/style.min.css');
+require('!!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('!!style-loader!css-loader!./js/style.min.css');
 
 // Import modules without parsing with script-loader,
 // !! to override all loaders
-require('!!script!core-js/client/shim.min.js');
-require('!!script!zone.js/dist/zone.js');
-require('!!script!reflect-metadata/Reflect.js');
-require('!!script!rxjs/bundles/Rx.js');
-require('!!script!@angular/core/bundles/core.umd.js');
-require('!!script!@angular/common/bundles/common.umd.js');
-require('!!script!@angular/compiler/bundles/compiler.umd.js');
-require('!!script!@angular/platform-browser/bundles/platform-browser.umd.js');
-require('!!script!@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js');
+require('!!script-loader!core-js/client/shim.min.js');
+require('!!script-loader!zone.js/dist/zone.js');
+require('!!script-loader!reflect-metadata/Reflect.js');
+require('!!script-loader!rxjs/bundles/Rx.js');
+require('!!script-loader!@angular/core/bundles/core.umd.js');
+require('!!script-loader!@angular/common/bundles/common.umd.js');
+require('!!script-loader!@angular/compiler/bundles/compiler.umd.js');
+require('!!script-loader!@angular/platform-browser/bundles/platform-browser.umd.js');
+require('!!script-loader!@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js');
 
-require('!!script!moment/min/moment.min.js');
-require('!!script!moment-timezone/builds/moment-timezone-with-data.min.js');
-require('!!script!web-animations-js/web-animations.min.js');
+require('!!script-loader!moment/min/moment.min.js');
+require('!!script-loader!moment-timezone/builds/moment-timezone-with-data.min.js');
+require('!!script-loader!web-animations-js/web-animations.min.js');
 
 // Import boot, resolve imports/requires, and pass through Babel
 require('./js/main');
